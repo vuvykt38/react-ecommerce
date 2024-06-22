5f00336897be87f4257f01e19145658e2684edb0
Vy Vu <40362144+vuvykt38@users.noreply.github.com>
2024-06-23 01:22:40 +0700

    add FB login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
-import { Contact } from "./pages/contact";
+import { Login } from "./pages/login";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
 
@@ -14,7 +14,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
-            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </Router>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="links">
         <Link to="/"> Shop </Link>
-        <Link to="/contact"> Contact </Link>
+        <Link to="/login"> Login </Link>
         <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
